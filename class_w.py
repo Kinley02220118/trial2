dd02a981bd2b1da34cada28fbcbe73ef414235d5
Kinley02220118 <02220118.cst@rub.edu.bt>
2024-05-14 00:59:14 +0600

    adding class_w.py file

---
+++ class_w.py
@@ -0,0 +1,19 @@
+class Dog:
+    def __init__(self, name, colour, age):
+        self.name = name
+        self.colour = colour
+        self.age = age
+
+dawa = Dog('dawa', 'blue', 2)
+nima = Dog('nima', 'black', 1)
+blacky = Dog('blacky', 'yellow', 3)
+
+print(dawa.name)
+print(dawa.colour)
+print(dawa.age)
+print(nima.name)
+print(nima.colour)
+print(nima.age)
+print(blacky.name)
+print(blacky.colour)
+print(blacky.age)
